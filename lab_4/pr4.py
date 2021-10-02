3047c80482cbb6afede1ed4beb22cd0fa44a5a30
rinagovell <90962076+rinagovell@users.noreply.github.com>
2021-10-02 13:43:34 +0300

    Add files via upload
    Improving code

--- lab_4/pr4.py
+++ lab_4/pr4.py
@@ -1,6 +1,12 @@
 a = int(input("Enter the first number (different from 0): "))
-b = int(input("Enter the second number (different from 0): "))
-print('\n', "The sum is equal to: ", a + b, '\n', 
-"The difference is equal to: ", abs(a - b), '\n', 
-"The product of squares of numbers is equal to: ", a * a * b * b, '\n',
-"The quotent of squares of numbers is equal to: ", (a * a) / (b * b), '\n')
+if a == 0:
+    print('Error')
+else: 
+    b = int(input("Enter the second number (different from 0): "))
+    if b == 0:
+        print('Error')
+    else:
+        print('\n', "The sum is equal to: ", a + b, '\n', 
+        "The difference is equal to: ", abs(a - b), '\n', 
+        "The product of squares of numbers is equal to: ", a * a * b * b, '\n',
+        "The quotent of squares of numbers is equal to: ", (a * a) / (b * b), '\n')
--- lab_4/pr5.py
+++ lab_4/pr5.py
@@ -1,6 +1,12 @@
 a = int(input("Enter the first number (different from 0): "))
-b = int(input("Enter the second number (different from 0): "))
-print('\n', "The sum of modules is equal to: ", abs(a) + abs(b), '\n', 
-"The difference of modules is equal to: ", abs(a) - abs(b), '\n', 
-"The product of modules of numbers is equal to: ", abs(a) * abs(b), '\n',
-"The quotent of modules of numbers is equal to: ", abs(a) / abs(b), '\n')
+if a == 0:
+    print('Error')
+else:
+    b = int(input("Enter the second number (different from 0): "))
+    if b == 0:
+        print('Error')
+    else:
+        print('\n', "The sum of modules is equal to: ", abs(a) + abs(b), '\n', 
+        "The difference of modules is equal to: ", abs(a) - abs(b), '\n', 
+        "The product of modules of numbers is equal to: ", abs(a) * abs(b), '\n',
+        "The quotent of modules of numbers is equal to: ", abs(a) / abs(b), '\n')
