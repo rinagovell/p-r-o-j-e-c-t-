c6dd4ef94371095c1439abc64bdf8c52c3ba4eb2
rinagovell <90962076+rinagovell@users.noreply.github.com>
2021-12-01 18:05:58 +0300

    laba 11
    исправление ошибки в 3 проге

--- lab_11/pr_3.cpp
+++ lab_11/pr_3.cpp
@@ -6,17 +6,26 @@ using namespace std;
 
 int main(){
     cout << "\n Задание 3. \n";
-    int a, b, c;
-    cout << "Введите координату точки A: ";
-    cin >> a;
-    cout << "Введите координату точки B: ";
-    cin >> b;
-    cout << "Введите координату точки C: ";
-    cin >> c;
-    if (abs(a - b) < abs(a - c)){
-        cout << "Точка B ближе к A. Расстояние от A до B равно: " << abs(a - b) << "\n";
+    int ax, ay, bx, by, cx, cy;
+    float ab, ac;
+    cout << "Введите координаты точки A на плоскости: \n";
+    cin >> ax;
+    cin >> ay;
+    cout << "Введите координаты точки B на плоскости: \n";
+    cin >> bx;
+    cin >> by;
+    cout << "Введите координаты точки C на плоскости: \n";
+    cin >> cx;
+    cin >> cy;
+    ab = abs(pow((bx - ax), 2) + pow((by - ay), 2));
+    ac = abs(pow((cx - ax), 2) + pow((cy - ay), 2));
+    if (ab < ac){
+        cout << "Точка B ближе к A. Расстояние от A до B равно: " << ab << "\n";
+    }
+    else if (ac < ab){
+        cout << "Точка C ближе к A. Расстояние от A до C равно: " << ac << "\n";
     }
     else{
-        cout << "Точка C ближе к A. Расстояние от A до C равно: " << abs(a - c) << "\n";
+        cout << "Точки B и C равноудалены от точки A \n";
     }
 }
